23bab1b824912db898f46ec1cf420fe668a47420
Asser <asseribrahim@cmail.carleton.ca>
2023-02-23 19:18:04 -0500

    added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -96,3 +96,10 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
