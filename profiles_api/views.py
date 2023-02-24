3a5d7d6bc8d1630650d9a247e64863c51cd48a5d
Asser <asseribrahim@cmail.carleton.ca>
2023-02-23 23:15:55 -0500

    Update views.py

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -103,3 +103,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
